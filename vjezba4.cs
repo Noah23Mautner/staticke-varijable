ff6df6cc67af257cb019d39e7d769d08dfc3a90d
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-01-09 13:06:13 +0100

    Add files via upload

---
+++ vjezba4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statičke_varijable
+{ 
+
+     class PrvaKlasa
+    {
+        private static int counter = 0;
+        private string naziv;
+
+        public PrvaKlasa(string naziv)
+        {
+            counter++;
+            this.naziv = naziv; 
+        }
+
+        public static int GetCounter(){ return counter;}
+        
+        public string GetNaziv() { return naziv; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa x = new PrvaKlasa("Prvi");
+            Console.WriteLine(x.GetNaziv());
+            Console.WriteLine("Vrijednost " + PrvaKlasa.GetCounter());
+            PrvaKlasa y = new PrvaKlasa("Drugi");
+            Console.WriteLine(y.GetNaziv());
+            Console.WriteLine("Vrijednost " + PrvaKlasa.GetCounter());
+            PrvaKlasa z = new PrvaKlasa("Treci");
+            Console.WriteLine(z.GetNaziv());
+            Console.WriteLine("Vrijednost " + PrvaKlasa.GetCounter());
+
+           
+            Console.ReadKey();
+
+        }
+    }
+}
+
